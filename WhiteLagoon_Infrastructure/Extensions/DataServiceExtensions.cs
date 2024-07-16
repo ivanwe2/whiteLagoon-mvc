65a2b2120f5a098a5c32140eb202cf8fe89597a0
Ivan Rusev <ivanrusev03@schoolmath.eu>
2024-07-16 15:36:20 +0300

    feat: create database, villa model and seed villa table

---
+++ WhiteLagoon_Infrastructure/Extensions/DataServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon_Infrastructure.Data;
+
+namespace WhiteLagoon_Infrastructure.Extensions
+{
+    public static class DataServiceExtensions
+    {
+        public static IServiceCollection AddDefaultDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(option => 
+                option.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            return services;
+        }
+    }
+}
